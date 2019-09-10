14d17d9ce1dc298af5d83c3a9c8893f5c5026179
Dipendra Dangol <dipendra.dangol11@gmail.com>
2019-09-10 10:11:08 +0300

    add city and country features

--- src/App.js
+++ src/App.js
@@ -1,31 +1,67 @@
 import React from 'react';
-import Weather from './components/weather';
 import Titles from './components/titles';
+import Weather from './components/weather';
 import Form from './components/form';
-import './App.css';
 
-const Apikey = '1f7e74434dacd114733c30d29635f23f';
-
-// app-component
-
-
+const API_KEY = '5aac476a0674676d51ece2175d1f3a86';
 
 class App extends React.Component {
-  getWeather = async () => {
-    const api_call = await fetch `http://api.openweathermap.org/data/2.5/box/city?bbox=12,32,15,37,10`;
-    const response = await api_call.json();
-    console.log(response);
-    }
-  render(){
-    return (
-      <div className="App">
-        <Titles/>
-        <Form loadWeather={this.getWeather}/>
-        <Weather/>
-      </div>
-    );
-  }
-  
-}
+	state = {
+		temperature: undefined,
+		city: undefined,
+		country: undefined,
+		humidity: undefined,
+		description: undefined,
+		error: undefined
+	}
 
+	getWeather = async (e) => {
+		e.preventDefault(); debugger
+
+		const city = e.target.elements.city.value;
+
+		const country = e.target.elements.country.value;
+			
+		const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
+		
+		const response = await api_call.json();
+		
+		console.log(response);
+
+		if (city && country) {
+			this.setState({
+				temperature: response.main.temp,
+				city: response.name,
+				country: response.sys.country,
+				humidity: response.main.humidity,
+				description: response.weather[0].description,
+				error: ""
+			}) 
+		} else {
+			this.setState({
+				error: "Please input valid data..."
+			})
+		}
+	}
+
+	render() {
+		return ( 
+			<div >
+				<Titles></Titles>
+				
+				<Form loadWeather={this.getWeather}>
+					
+				</Form>
+
+				<Weather
+					temperature={this.state.temperature}
+					city={this.state.city}
+					country={this.state.country}
+					humidity={this.state.humidity}
+					description={this.state.description}
+					error={this.state.error} />
+			</div>
+		)
+	}
+}
 export default App;
